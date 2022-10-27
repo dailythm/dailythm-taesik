995c6b6d700656dd508b4676867ce60a9d88462f
Taesik yoon <taesikyoon97@naver.com>
2022-10-28 00:09:13 +0900

    분수의 덧셈

---
+++ 1주차/08.py
@@ -0,0 +1,7 @@
+import math
+def solution(denum1, num1, denum2, num2):
+    denum = [(denum1*num2)+(denum2*num1)]
+    num =[num1*num2]
+    div= math.gcd(denum[0],num[0])
+    answer = [denum[0]/div,num[0]/div]
+    return answer
