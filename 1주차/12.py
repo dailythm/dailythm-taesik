5003445f7b162a14beff386a8197c60698d4feba
Taesik yoon <taesikyoon97@naver.com>
2022-10-28 21:24:02 +0900

    최빈값 구하기

---
+++ 1주차/12.py
@@ -0,0 +1,13 @@
+def solution(array):
+    if len(array) == 1:
+        return array[0]
+    answer = []
+    setList = list(set(array))
+    for x in setList:
+        answer.append(array.count(x))
+    if answer.count(max(answer)) > 1:
+        return -1
+    index = answer.index(max(answer))
+    return setList[index]
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120812
