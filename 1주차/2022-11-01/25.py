44c244c19bcb9c62093adef29a4cc13f0e0b4abd
Taesik yoon <taesikyoon97@naver.com>
2022-11-01 12:02:34 +0900

    각도기

---
+++ 1주차/2022-11-01/25.py
@@ -0,0 +1,13 @@
+def solution(angle):
+    answer = 0
+    if 0 < angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif 90 < angle < 180:
+        return 3
+    elif angle == 180:
+        return 4
+    return answer
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120829
