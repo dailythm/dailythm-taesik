dbc4174b8476e80b4153de0268a45a2feabc7270
Taesik yoon <taesikyoon97@naver.com>
2022-11-01 12:15:12 +0900

    양꼬치

---
+++ 1주차/2022-11-01/26.py
@@ -0,0 +1,7 @@
+def solution(n, k):
+    if n < 10:
+        return (n*12000)+(k*2000)
+    answer = n*12000+((k-n//10)*2000)
+    return answer
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/120830
